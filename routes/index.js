676501ae445afad4ebeb67f92ca22bcbc373b752
Alex Supkay <asupkay1124@gmail.com>
2018-02-17 15:04:21 -0500

    Finish up stock tracker

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const pkg_json = require('../package.json')
 const turbo = require('turbo360')({site_id:pkg_json.app})
 const vertex = require('vertex360')({site_id:pkg_json.app})
 const router = vertex.router()
+const https = require("https")
+const url = require('url')
 
 /*  This is the home route. It renders the index.mustache page from the views directory.
 	Data is rendered using the Mustache templating engine. For more
@@ -20,12 +22,72 @@ router.get('/stocks', function(req, res) {
     if(req.vertexSession.user) {
         turbo.fetchUser(req.vertexSession.user.id)
         .then(data => {
-            res.render('stocks', {stockinput: data.stockinput})
+            getStockInfo(data.stockinput).then(stockdata => {
+                let stockprices = []
+                for(let i = 0; i < data.stockinput.length; i++) {
+                    stockprices.push({name: data.stockinput[i], price: stockdata[i]})
+                }
+                console.log(stockprices);
+                res.render('stocks', {stockinput: stockprices})
+            });
         })
     } else {
         res.redirect('/')
     }
 })
+
+getStockInfo = (stockinput) => {
+    let promises = [];
+   
+    stockinput.map((item) => {
+        let URL = url.parse('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + item + '&apikey=YOUR_API_KEY')
+        let promise = getStockPrice(URL)
+        promises.push(promise)
+    });
+        
+    return Promise.all(promises).then((values) => {
+        return values
+    })
+}
+
+function getStockPrice(url) {
+    let promise = new Promise(
+    function(resolve, reject) {
+            
+        const option = {
+            hostname: url.host,
+            path: url.path,
+        };
+
+        //Call the https get request
+        let getRequest = https.get(option, (res) => {
+            let fulldata;
+            res.on('data', (d) => {
+                if(fulldata) {
+                    fulldata += d;
+                } else {
+                    fulldata = d;
+                }
+            });
+            res.on('end', () => {
+                let price; 
+
+                //Parse out the JSON data and grab the price from it
+                try {
+                    let stockJSONData = JSON.parse(fulldata)
+                    if(stockJSONData) {
+                        price = stockJSONData["Time Series (Daily)"][Object.keys(stockJSONData["Time Series (Daily)"])[0]]['4. close']
+                    }   
+                } catch (error) {
+                    price = "parsing error";
+                }
+                //resolve the promsie to the price information
+                resolve(price);
+            });
+        });
+    });
+    return promise;
+}
 
 router.post('/stocks', function(req, res) {
     turbo.fetchUser(req.vertexSession.user.id)
